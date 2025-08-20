dd2165a389a333e525c266dcb5d77e2d6e3e75f1
allencando19-prog <allencando19@gmail.com>
2025-08-20 17:59:49 +0800

    feat: add basic variables and data types
    - Set up JavaScript environment and browser console
    - Implement let, const, var variable declarations
    - Add data type exploration with typeof operator
    - Establish development workflow and best practices

--- 01-fundamentals-part-1/starter/starter/script.js
+++ 01-fundamentals-part-1/starter/starter/script.js
@@ -1,23 +1,110 @@
 
-// JavaScript Fundamentals - Hour 1
-// My first JavaScript code!
+// // JavaScript Fundamentals - Hour 1
+// // My first JavaScript code!
 
 
-let js = "amazing";
-console.log(40 + 8 + 23 - 10);
-console.log(js);
+// let js = "amazing";
+// console.log(40 + 8 + 23 - 10);
+// console.log(js);
 
-console.log("=== VARIABLES ===");
+// console.log("=== VARIABLES ===");
 
-let firstName = "Will Allen";
-console.log(firstName);
+// let firstName = "Will Allen";
+// console.log(firstName);
 
-let age = 30;
-console.log(age);
-age = 31; 
-console.log(age);
+// let age = 30;
+// console.log(age);
+// age = 31; 
+// console.log(age);
 
-const birthYear = 1991;
-console.log(birthYear);
+// const birthYear = 1991;
+// console.log(birthYear);
 
-const PI = 3.1415;
+// const PI = 3.1415;
+// console.log(PI); 3.145
+
+// var job = "programmer";
+// job = "teacher";
+// console.log(job); 'teacher'
+
+// const country = "Philippines";
+// const language = "Filipino";
+
+// age = 22;
+// age = 23;
+
+// console.log("=== DATA TYPES ===");
+
+// let id = 12345;
+// console.log(id); 12345
+// console.log(typeof id); 'number'
+
+// let lastName = "Allen";
+// console.log(lastName); 'Allen'
+// console.log(typeof lastName); 'string'
+
+// let javaScriptIsFun = true;
+// console.log (javaScriptIsFun); true
+// console.log(typeof javaScriptIsFun); 'boolean'
+
+// let year;
+// console.log(year); undefined
+// console.log(typeof year); 'undefined'
+
+// let dynamicVariable = 40;
+// console.log(dynamicVariable, typeof dynamicVariable);
+
+// dynamicVariable = "I am now a string!";
+// console.log(dynamicVariable, typeof dynamicVariable);
+// dynamicVariable = true;
+
+console.log("=== MATH OPERATORS");
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+
+console.log("Math operations");
+console.log("Addition", 10 + 5);
+console.log("Subtraction", 20 - 8);
+console.log("Multiplication", 4 * 7);
+console.log("Division", 15 / 3);
+console.log("Exponentation", 2 ** 3);
+
+const firstName = "Allen";
+const lastName = "Cando";
+console.log("Concatenation",firstName + " " + lastName);
+
+console.log("Hello" + "World" + "!");
+
+console.log("=== ASSIGNMENT OPERATIONS ===");
+
+let x = 10 + 5;
+console.log("x starts as:", x);
+
+x += 10;
+console.log("x starts as:", x);
+
+x *= 4;
+console.log("x starts as:", x);
+
+x /= 2;
+console.log("x starts as:", x);
+
+x++;
+console.log("x starts as:", x);
+
+x--;
+console.log("x starts as:", x);
+
+
+
+
+
+
+
+
+
